e57079b6f78a3fde60d9cf625cb93beab7ee471d
Priscila Roza <priscilarozasilva@gmail.com>
2021-12-14 15:59:58 -0300

    feature[progresso.component and tentativas.componentcreated]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 
 @NgModule({
-  declarations: [AppComponent, TopoComponent, PainelComponent],
+  declarations: [AppComponent, TopoComponent, PainelComponent, TentativasComponent, ProgressoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
