827169e07edc39057779c70503ecf65ba9290acb
Priscila Roza <priscilarozasilva@gmail.com>
2021-12-14 15:48:30 -0300

    feature[painel.component - created]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
 
 
 @NgModule({
-  declarations: [AppComponent, TopoComponent],
+  declarations: [AppComponent, TopoComponent, PainelComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
